6084a6c395193b2bce8fee8dd331809dd57c32c2
Caine Winters <caine.winters@outlook.com>
2023-10-23 14:50:50 -0400

    Initial init

---
+++ src/components/portfolio.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function Portfolio() {
+    const [portfolio, setPortfolio] = useState();
+    
+
+    return (
+        <div>
+            <h1>The mighty migthy PORTFOLIO</h1>
+        </div>
+    );
+}
+
+export default Portfolio;
